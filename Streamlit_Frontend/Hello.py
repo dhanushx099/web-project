e8ea0c46ba6c79cb65047492ecc421fc416046d4
dhanushx099 <dhanushsanju99@gmail.com>
2025-04-15 10:27:36 +0530

    Add files via upload

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="OPTIONS",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Diet Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    Smart Meal Planner: Personalized Diets with Nutritional Insights
+    
+    """
+)
